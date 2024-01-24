dfbd0294128c7250e9a149e60119c86e9d697d2c
Valerie <88196359+Valerie6048@users.noreply.github.com>
2024-01-24 17:17:47 +0700

    Update Titanic_Dashboard.py

--- Titanic_Dashboard.py
+++ Titanic_Dashboard.py
@@ -4,6 +4,7 @@ import numpy as np
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import xgboost as xgb
 import matplotlib.pyplot as plt
 import plotly.express as px
 import pickle
