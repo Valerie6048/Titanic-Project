cf68c5ad9afb9e3d1cfb03e1cdd9d78c34581570
Valerie <88196359+Valerie6048@users.noreply.github.com>
2024-01-31 23:05:53 +0700

    Update Titanic_Dashboard.py

--- Titanic_Dashboard.py
+++ Titanic_Dashboard.py
@@ -54,7 +54,8 @@ with tabs1:
     sns.barplot(x='Sex', y='Survived', data=train_df, ci=None, palette='rocket', ax=ax)
     ax.set_ylabel('Survival Probability')
     ax.set_title('Survival Probability by Gender')
-    ax.set_xticklabels(x, ['Men', 'Women'])
+    ax.set_xticks(x)
+    ax.set_xticklabels(['Men', 'Women'])
     st.pyplot(fig)
 
     st.subheader('Survival Probability by Passenger Class Visualization')
